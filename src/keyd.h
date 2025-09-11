keyd-project/keyd-fork

Use autotools (autoconf, automake) to build keyd

src/keyd.h
@@ -6,6 +6,8 @@
 #ifndef KEYD_H_
 #define KEYD_H_
 
+#include <autoconfig.h>
+
 #include <assert.h>
 #include <ctype.h>
 #include <dirent.h>

rhansen/keyd

src/keyd.h
@@ -6,6 +6,8 @@
 #ifndef KEYD_H_
 #define KEYD_H_
 
+#include <sys_config.h>
+
 #include <assert.h>
 #include <ctype.h>
 #include <dirent.h>